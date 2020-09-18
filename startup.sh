a3e4cb32a73e7936e022644b3dc2140d3ed1ac06
Carlos <carlos.bergamasco@gmail.com>
2020-09-18 00:30:17 -0300

    Introducing startup script and change config

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# mount data HD
+#
+# /etc/init.d/my_init.sh
+
+# mount data HD
+# id needed put this command above to create folder first
+# mkdir /media/carlos/hd
+#mount /dev/sda6 /media/carlos/hd/
+exec google-chrome-stable & 
+exec xreader "/home/carlos/Documentos/pessoal/learn/books/Domain Driven Design Quickly.pdf" &
+exec xed ~/.config/i3/startup.sh &
+exec xed ~/.config/i3/config &
+exec xed ~/leitura

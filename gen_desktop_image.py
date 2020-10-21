d63c2a8c1ec88cf66f6aa9ad30d1a3a4423296d5
Carlos <carlos.bergamasco@gmail.com>
2020-10-21 17:28:00 -0300

    Change configs to generate desktop image

--- gen_desktop_image.py
+++ gen_desktop_image.py
@@ -11,26 +11,41 @@ END_READING = "### End Read for Desktop Image ###"
 CHANGE_STRING_LIST = [("bindsym ", ""), ("$mod", "Win"), (" exec ", " => ")]
 
 def main():
-    image = text_image('/home/tiwork/.config/i3/config', '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf')
+    image = text_image('/home/tiwork/.config/i3/config', '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf')
     image.save('/home/tiwork/.config/i3/desktop.png')
 
-def command_filter(line):
+def command_filter(line, prev_line):
     for item in CHANGE_STRING_LIST:
         line = line.replace(item[0], item[1])
+
+    if prev_line.startswith("#"):
+        line = add_comment(line, prev_line.split('#')[1])
+
     return line
+
+def add_comment(line, prev_line):
+    command = line.split("=>")
+    description = prev_line.replace('#', '')
+    return command[0].replace('\n', '') + "=>" + description
 
 def get_eligible_lines(arr):
     is_reading = False
     lines_list = []
-    print("Lines length: ")
-    print(len(arr))
+    prev_line = ''
+    cur_line = ''
+
+    lines_list.append("--- Desktop Main Shortcuts ---")
+    lines_list.append("______________________________")
     for line in arr:
         if line.rstrip() == INIT_READING:
             is_reading = True
         elif line.rstrip() == END_READING:
             is_reading = False
         elif is_reading == True:
-            lines_list.append(command_filter(line))
+            cur_line = command_filter(line, prev_line)
+            if not cur_line.startswith('#'):
+                lines_list.append(cur_line)
+            prev_line = line
     return lines_list
 
 def text_image(text_path, font_path=None):
@@ -47,7 +62,7 @@ def text_image(text_path, font_path=None):
         lines = tuple(l.rstrip() for l in get_eligible_lines(text_file.readlines()))
 
     # choose a font (you can see more detail in my library on github)
-    large_font = 20  # get better resolution with larger size
+    large_font = 30  # get better resolution with larger size
     font_path = font_path or 'cour.ttf'  # Courier New. works in windows. linux may need more explicit path
     try:
         font = PIL.ImageFont.truetype(font_path, size=large_font)
@@ -56,21 +71,23 @@ def text_image(text_path, font_path=None):
         print('Could not use chosen font. Using default.')
 
     # make the background image based on the combination of font and lines
-    pt2px = lambda pt: int(round(pt * 96.0 / 72))  # convert points to pixels
+    pt2px = lambda pt: int(round(pt * 96.0 / 36))  # convert points to pixels
     max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
     # max height is adjusted down because it's too large visually for spacing
     test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
+
+    print(len(lines))
     height = max_height * len(lines)  # perfect or a little oversized
     width = int(round(max_width))  # a little oversized
     image = PIL.Image.new(grayscale, (width, height), color=PIXEL_OFF)
     draw = PIL.ImageDraw.Draw(image)
 
     # draw each line of text
-    vertical_position = 5
-    horizontal_position = 5
-    line_spacing = int(round(max_height * 0.8))  # reduced spacing seems better
+    vertical_position = 3
+    horizontal_position = 3
+    line_spacing = int(round(max_height * 0.6))  # reduced spacing seems better
     for line in lines:
         draw.text((horizontal_position, vertical_position),
                   line, fill=PIXEL_ON, font=font)
--- startup.sh
+++ startup.sh
@@ -11,16 +11,14 @@
 ######################################
 
 #exec ij &
-#exec xreader "/home/carlos/Documentos/pessoal/learn/books/Domain Driven Design Quickly.pdf" &
+#exec xed ~/.config/i3/startup.sh &
+#exec xed ~/.config/i3/config &
+#exec xed ~/leitura &
+#exec xed ~/Documents/study_viaVarejo &
 exec python3 ~/.config/i3/gen_desktop_image.py &
 feh --bg-scale ~/.config/i3/desktop.png &
 exec google-chrome-stable & 
 exec brave-browser &
-exec xed ~/.config/i3/startup.sh &
-exec xed ~/.config/i3/config &
-exec xed ~/leitura &
-exec xed ~/Documents/study_viaVarejo &
 exec xed ~/Documents/study &
 exec xfce4-terminal &
-exec xed ~/.xsession &
 

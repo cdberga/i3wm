2f30699376740621a588349579d6c20bf67e7e86
Carlos <carlos.bergamasco@gmail.com>
2020-11-09 17:17:54 -0200

    Add config and startup work changes

--- startup_work.sh
+++ startup_work.sh
@@ -3,3 +3,5 @@
 exec python3 /home/carlosbergamasco/.config/i3/gen_desktop_image.py &
 feh --bg-scale /home/carlosbergamasco/.config/i3/desktop.png &
 exec gnome-terminal &
+exec gedit ~/Documents/infos_loggi &
+exec google-chrome https://meet.google.com/kti-brbu-ojb

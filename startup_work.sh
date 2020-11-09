1536e4064baf7a7e7a10219cf19754ef9e258c33
Carlos <carlos.bergamasco@gmail.com>
2020-11-09 09:44:00 -0200

    Update config and startup for work

--- startup_work.sh
+++ startup_work.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-exec python3 ~/.config/i3/gen_desktop_image.py &
-feh --bg-scale ~/.config/i3/desktop.png &
+exec python3 /home/carlosbergamasco/.config/i3/gen_desktop_image.py &
+feh --bg-scale /home/carlosbergamasco/.config/i3/desktop.png &
 exec gnome-terminal &

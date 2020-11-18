cd7bcc86dc20f8bf150a9a73c0c5c47a4e73914d
Carlos <carlos.bergamasco@gmail.com>
2020-11-18 10:24:47 -0200

    chore(config): Remove startup view for edit files
    Remove config, infos_loggi and startup_work that was for editting.
    
    No issue

--- startup_work.sh
+++ startup_work.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 feh --bg-scale /home/carlosbergamasco/.config/i3/desktop.png &
 exec gnome-terminal &
-exec gedit ~/Documents/infos_loggi &
-exec gedit ~/.config/i3/config &
-exec gedit ~/.config/i3/startup_work.sh &
 exec google-chrome

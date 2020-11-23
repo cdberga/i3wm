75333c4a639c731d33df86c26148c6fefcfab849
Carlos <carlos.bergamasco@gmail.com>
2020-11-23 19:06:18 -0200

    Change firefox link and try make update script to restart i3 (commented)

--- update_config.sh
+++ update_config.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#mv startup_work.sh startup_w1.sh &&
+#i3-msg restart &&
+#>startup_work.sh &&
+#rm -rf startup_work.sh &&
+#mv startup_w1.sh startup_work.sh
 cp config_$I3_ENV config &
 python3 /home/carlosbergamasco/.config/i3/gen_desktop_image.py &
 feh --bg-scale /home/carlosbergamasco/.config/i3/desktop.png

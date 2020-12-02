8fe7c70093a264b28261d20d10908e09d8bc3b7b
Carlos <carlos.bergamasco@gmail.com>
2020-12-02 19:16:58 -0200

    Update startup work file, adding a new screen resolution and some more system shortcuts

--- startup_work.sh
+++ startup_work.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#exec gedit "/home/carlosbergamasco/Documents/trilha de engenharia" &
+#exec google-chrome
 feh --bg-scale /home/carlosbergamasco/.config/i3/desktop.png &
 exec gnome-terminal &
-exec google-chrome

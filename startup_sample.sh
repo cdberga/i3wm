6f96b66244e76976da7a8001e4eaa695c627fd2d
Carlos <carlos.bergamasco@gmail.com>
2021-02-03 18:43:59 -0200

    Add sample startup file and remove home and work startup files from index

---
+++ startup_sample.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+feh --bg-scale $HOME/.config/i3/desktop.png &
+exec gnome-terminal &
--- startup_work.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#exec gedit "$HOME/Documents/trilha de engenharia" &
-feh --bg-scale $HOME/.config/i3/desktop.png &
-exec gnome-terminal
